a749338b375ab28728ddbfbd08319483e0b0b126
brecabre <bravomantonio@gmail.com>
2020-05-29 22:15:34 +0200

    Add files via upload

--- recursos/Global/Global.gd
+++ recursos/Global/Global.gd
@@ -1,14 +1,41 @@
 extends Node
 
-var vidasIniciales = 3
-var puntuacion = 0
-var monedas = 0
-var vidas = vidasIniciales
-var nivel = 1
+#var vidasIniciales : int
+var puntuacion : int
+var monedas : int
+var vidas : int
+var nivel : int
+var carpeta_niveles : String
+var numero_max_niveles : int
 
 func _ready():
-	print("puntuación: ", puntuacion, ", vidas: ", vidas , ", nivel", nivel, "monedas: ", monedas)
-	pass
+	nivel = 1
+	puntuacion = 0
+	monedas = 0
+	vidas = 3
+	
+	carpeta_niveles = "res://Juego/Escenarios/Niveles"
+	numero_max_niveles = (lista_archivos_en_directorio(carpeta_niveles).size())
+
+
+func lista_archivos_en_directorio(path):
+    var files = []
+    var dir = Directory.new()
+    dir.open(path)
+    dir.list_dir_begin()
+
+    while true:
+        var file = dir.get_next()
+        if file == "":
+            break
+        elif not file.begins_with("."):
+            files.append(file)
+
+    dir.list_dir_end()
+
+    return files
+
+
 func _unhandled_input(event):
     if event is InputEventKey:
         if event.pressed and event.scancode == KEY_ESCAPE:
